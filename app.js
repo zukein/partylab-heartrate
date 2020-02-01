a2df4f4f6e19b9b2cf30042cc6266ae087f28699
Tanaka <zukunfteiner@me.com>
2020-02-01 22:12:51 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+// https://webbluetoothcg.github.io/demos/heart-rate-sensor/
+
+var canvas = document.querySelector('canvas');
+var statusText = document.querySelector('#statusText');
+
+statusText.addEventListener('click', function() {
+  statusText.textContent = '設定中...';
+  heartRates = [];
+  heartRateSensor.connect()
+  .then(() => heartRateSensor.startNotificationsHeartRateMeasurement().then(handleHeartRateMeasurement))
+  .catch(error => {
+    statusText.textContent = error;
+  });
+});
+
+function handleHeartRateMeasurement(heartRateMeasurement) {
+  heartRateMeasurement.addEventListener('characteristicvaluechanged', event => {
+    var now = new Date();
+    var heartRateMeasurement = heartRateSensor.parseHeartRate(event.target.value);
+    statusText.innerHTML = now.toLocaleTimeString() + ' ' + heartRateMeasurement.heartRate + 'bpm' ;
+    heartRates.push(heartRateMeasurement.heartRate);
+    drawWaves();
+    ', ' + console.log(now.toLocaleTimeString() + ', ' + heartRateMeasurement.heartRate + ', bpm' )
+  });
+}
+
+var heartRates = [];
+var mode = 'bar';
+
+canvas.addEventListener('click', event => {
+  mode = mode === 'bar' ? 'line' : 'bar';
+  drawWaves();
+});
+
+function drawWaves() {
+  requestAnimationFrame(() => {
+    canvas.width = parseInt(getComputedStyle(canvas).width.slice(0, -2)) * devicePixelRatio;
+    canvas.height = parseInt(getComputedStyle(canvas).height.slice(0, -2)) * devicePixelRatio;
+
+    var context = canvas.getContext('2d');
+    var margin = 2;
+    var max = Math.max(0, Math.round(canvas.width / 11));
+    var offset = Math.max(0, heartRates.length - max);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.strokeStyle = '#ff9eb5';
+    if (mode === 'bar') {
+      for (var i = 0; i < Math.max(heartRates.length, max); i++) {
+        var barHeight = Math.round(heartRates[i + offset ] * canvas.height / 200);
+        context.rect(5 * i + margin, canvas.height - barHeight, margin, Math.max(0, barHeight - margin));
+        context.stroke();
+      }
+    } else if (mode === 'line') {
+      context.beginPath();
+      context.lineWidth = 100;
+      context.lineJoin = 'round';
+      context.shadowBlur = '1';
+      context.shadowColor = '#333';
+      context.shadowOffsetY = '1';
+      for (var i = 0; i < Math.max(heartRates.length, max); i++) {
+        var lineHeight = Math.round(heartRates[i + offset ] * canvas.height / 200);
+        if (i === 0) {
+          context.moveTo(11 * i, canvas.height - lineHeight);
+        } else {
+          context.lineTo(11 * i, canvas.height - lineHeight);
+        }
+        context.stroke();
+      }
+    }
+  });
+}
+
+window.onresize = drawWaves;
+
+document.addEventListener("visibilitychange", () => {
+  if (!document.hidden) {
+    drawWaves();
+  }
+});
